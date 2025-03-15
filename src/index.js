62ac57535a6d7044d3400ebf0a771c3a76b1557b
kvngspice <sampelumibiz@gmail.com>
2025-03-15 02:05:45 +0100

    Updated files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";  // Ensure correct import for React 18
+import "bootstrap/dist/css/bootstrap.min.css";  // This should be before App import
 import App from "./App";  // Ensure App import is at the top
-import "bootstrap/dist/css/bootstrap.min.css";  // Ensure styles are imported after libraries
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
